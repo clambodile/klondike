377647b200de49a6e904c4dc205b7b08aa7b76e4
John Jacob <jacobjohnrobert@gmail.com>
2020-10-18 14:16:49 -0500

    Use proper terminology; implement Current, Next, Draw methods on Stock.

--- klondike.go
+++ klondike.go
@@ -2,33 +2,55 @@ package klondike
 
 import "./cards"
 
-type Column []cards.Card
+type Column []*cards.Card
 
-type Stack map[int][]cards.Card
+type Stack map[int][]*cards.Card
 
-type DrawPile struct {
-	Pile []cards.Card
+type Stock struct {
+	Pile []*cards.Card
 	I    int
 }
 
+func (stock *Stock) Current() *cards.Card {
+	return stock.Pile[stock.I]
+}
+
+func (stock *Stock) Next() {
+	stock.I++
+	if stock.I == len(stock.Pile) {
+		stock.I = 0
+	}
+}
+
+func (stock *Stock) Draw() *cards.Card {
+	defer func() {
+		stock.Pile = append(stock.Pile[:stock.I], stock.Pile[stock.I + 1:]...)
+		stock.I--
+		if stock.I == -1 {
+			stock.I = len(stock.Pile) - 1
+		}
+	}()
+	return stock.Current()
+}
+
 type GameState struct {
-	Stacks [4]Stack
-	Columns [7]Column
-	DrawPile DrawPile
+	Foundations [4]Stack
+	Tableau [7]Column
+	Stock Stock
 }
 
 func (gameState *GameState) Init() *GameState {
 	deck := &cards.Deck{}
-	//index of next card to draw
-	dI := 0
 	deck.Init()
 	deck.Shuffle()
-	var columns [7]Column
+	gameState.Tableau = [7]Column{}
 	for i := 0 ; i < 7; i++ {
-		for j := 0; j <= i + 1; j++ {
-			columns[i] = append(columns[i], deck.Pile[dI])
-			dI++
+		for j := 0; j < i + 1; j++ {
+			gameState.Tableau[i] = append(gameState.Tableau[i], deck.Draw())
 		}
+	}
+	for ; deck.I < len(deck.Pile); {
+		gameState.Stock.Pile = append(gameState.Stock.Pile, deck.Draw())
 	}
 	return &GameState{}
 }
---
+++ klondike_test.go
@@ -0,0 +1,88 @@
+package klondike
+
+import (
+	"./cards"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStock_Current(t *testing.T) {
+	t.Run("Shows the current available card in the stock", func(t *testing.T) {
+		deck := &cards.Deck{}
+		deck.Init()
+		stock := Stock{
+			Pile: deck.Pile[:24],
+			I: 0,
+		}
+		assert.Equal(t, "AS", stock.Current().String())
+	})
+}
+
+func TestStock_Next(t *testing.T) {
+	t.Run("Cycles to the next card in the stock", func(t *testing.T) {
+		deck := &cards.Deck{}
+		deck.Init()
+		stock := Stock{
+			Pile: deck.Pile[:24],
+			I: 0,
+		}
+		assert.Equal(t, "AS", stock.Current().String())
+		stock.Next()
+		assert.Equal(t, "2S", stock.Current().String())
+	})
+	t.Run("Returns to beginning after reaching the end.", func(t *testing.T) {
+		deck := &cards.Deck{}
+		deck.Init()
+		stock := Stock{
+			Pile: deck.Pile[:],
+			I: 0,
+		}
+		assert.Equal(t, "AS", stock.Current().String())
+		for i := 0; i < 51; i++ {
+			stock.Next()
+		}
+		assert.Equal(t, "KD", stock.Current().String())
+		stock.Next()
+		assert.Equal(t, "AS", stock.Current().String())
+	})
+}
+
+func TestStock_Draw(t *testing.T) {
+	t.Run("Returns the current card", func(t *testing.T) {
+		deck := &cards.Deck{}
+		deck.Init()
+		stock := Stock{
+			Pile: deck.Pile[:24],
+			I: 0,
+		}
+		card := stock.Draw()
+		assert.Equal(t, "AS", card.String())
+	})
+	t.Run("Removes the drawn card from the stock.", func(t *testing.T) {
+		deck := &cards.Deck{}
+		deck.Init()
+		stock := Stock{
+			Pile: deck.Pile[:24],
+			I: 0,
+		}
+		drawn := stock.Draw()
+		assert.Len(t, stock.Pile, 23)
+		for _, card := range stock.Pile {
+			assert.NotEqual(t, drawn.String(), card.String())
+		}
+	})
+}
+
+func TestGameState_Init(t *testing.T) {
+	gameState := &GameState{}
+	gameState.Init()
+
+	t.Run("Columns of tableau consist of 1, 2, 3, 4, 5, 6, 7 cards.", func(t *testing.T) {
+		for i := 0; i < 7; i++ {
+			assert.Len(t, gameState.Tableau[i], i + 1)
+		}
+	})
+	t.Run("Stock contains 24 cards.", func(t *testing.T) {
+		assert.Len(t, gameState.Stock.Pile, 24)
+	})
+}
